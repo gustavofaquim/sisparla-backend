dd0003537eda9ce117a0bdca65653baed989d003
Gustavo Faquim <gustavofaquim408@gmail.com>
2023-11-19 18:47:39 -0300

    feat: rota para imagens

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import mensagemController from '../controllers/mensagemController.js';
 import upload from "../config/multer.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 const router = express.Router();
 
+// Adicionando a configuração para servir arquivos estáticos
+const staticPath = path.join(__dirname, '../uploads');
+router.use('/uploads', express.static(staticPath));
+
+
 router
     .route("/send")
     .post(upload.array("arquivos"), (req, res) => mensagemController.send(req, res)); 
+
 
 export default router;
