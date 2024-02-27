bebfd785fe7af905b20618ed46153384658ac37d
Gustavo Faquim <gustavofaquim408@gmail.com>
2024-02-27 15:12:37 -0300

    fix: corrigido erro ao analisar token
    Ao acessar diretamente as URLs da API um erro de replace era exibido, foi adicionado um tratamento de erro.

--- middlewares/verificarToken.js
+++ middlewares/verificarToken.js
@@ -4,35 +4,42 @@ import revokedTokens from "./revokedTokens.js";
 
 const verificarToken = (req, res, next) => {
 
-  const tokenObtido = req.headers.authorization;
-  const token = tokenObtido.replace('Bearer ', '');
-  
-  if (!token) {
-    return res.status(401).json({ msg: 'Token não fornecido' });
-  }
-
-  // Verifica se o token foi revogado
-  if (revokedTokens.has(token)) {
-    //console.log('Toke revogado');
-    return res.status(401).json({ msg: 'Token revogado' });
-  }
-
-
-  jwt.verify(token,'secreto', (err, decoded) => {
+  try {
     
-    if (err) {
-      if (err.name === 'TokenExpiredError') {
-        console.log('Token expirado:', err.message);
-        return res.status(401).json({ msg: 'Token expirado' });
-      }
-
-      console.log('Erro ao verificar o token:', err.message);
-      return res.status(401).json({ msg: 'Token inválido' });
+    const tokenObtido = req.headers.authorization;
+    const token = tokenObtido.replace('Bearer ', '');
+    
+    if (!token) {
+      return res.status(401).json({ msg: 'Token não fornecido' });
     }
-    
-    req.usuario = decoded;
-    next();
-  });
+
+    // Verifica se o token foi revogado
+    if (revokedTokens.has(token)) {
+      //console.log('Toke revogado');
+      return res.status(401).json({ msg: 'Token revogado' });
+    }
+
+
+    jwt.verify(token,'secreto', (err, decoded) => {
+      
+      if (err) {
+        if (err.name === 'TokenExpiredError') {
+          console.log('Token expirado:', err.message);
+          return res.status(401).json({ msg: 'Token expirado' });
+        }
+
+        console.log('Erro ao verificar o token:', err.message);
+        return res.status(401).json({ msg: 'Token inválido' });
+      }
+      
+      req.usuario = decoded;
+      next();
+    });
+
+  } catch (error) {
+    res.json('Você não tem permissão para executar essa ação ou não está autenticado.');
+    console.log('Você não tem permissão para executar essa ação ou não está autenticado.')
+  }
 };
 
 export default verificarToken;
