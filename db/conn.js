760954299120bffb049b3fc80201545292bf9567
Gustavo Faquim <gustavofaquim408@gmail.com>
2024-03-03 15:41:05 -0300

    add: adicionado tratamento de erro na conexão com o banco

--- db/conn.js
+++ db/conn.js
@@ -5,10 +5,9 @@ dotenv.config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT } = process.env;
 
-async function main(){
+async function connectToDatabase() {
     try {
-        
-        const sequelize = new Sequelize(DB_NAME,DB_USERNAME,DB_PASSWORD, {
+        const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
             host: DB_HOST,
             dialect: DB_DIALECT,
             logging: false,
@@ -16,10 +15,26 @@ async function main(){
 
         await sequelize.authenticate();
         return sequelize;
-
     } catch (error) {
         console.log(`Erro ao conectar ao banco: ${error}`);
+        return null; // Retornar null em caso de falha na conexão
     }
+}
+
+async function main() {
+    let sequelizeInstance = null;
+
+    while (!sequelizeInstance) {
+        sequelizeInstance = await connectToDatabase();
+
+        if (!sequelizeInstance) {
+            const currentDate = new Date().toLocaleString();
+            console.log(`[${currentDate}] Tentando novamente em 5 minutos...`);
+            await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
+        }
+    }
+
+    return sequelizeInstance;
 }
 
 export default await main();
