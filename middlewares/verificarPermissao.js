a7b096192fac81bd71c877235f97daa494bd23d5
Gustavo Faquim <gustavofaquim408@gmail.com>
2024-03-27 17:26:34 -0300

    feat: ajustando verificação de permissão

--- middlewares/verificarPermissao.js
+++ middlewares/verificarPermissao.js
@@ -2,7 +2,7 @@ const verificarPermissao = (requiredPermission) => {
     return (req, res, next) => {
     
         const { usuario } = req;
-        if (!usuario || !usuario.regra || !(usuario.regra = requiredPermission)) {
+        if (!usuario || !usuario.regra || !(usuario.regra <= requiredPermission)) {
             return res.status(403).json({ msg: 'Você não tem permissão para acessar este recurso.' });
         }
      
